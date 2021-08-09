bbf06341228637a5a453d3e22cdc4c9625b92be3
Praghadiesh <praghadieshnikhil234@gmail.com>
2021-08-09 17:09:27 +0530

    implement conversation search feature

--- client/src/components/messenger/sidebar/Sidebar.js
+++ client/src/components/messenger/sidebar/Sidebar.js
@@ -1,17 +1,23 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import onClickOutside from 'react-onclickoutside';
 import { AuthenticationContext } from '../../../contexts/auth.context';
 import { SocketContext } from '../../../contexts/socket.context';
 import { MessengerContext } from '../../../contexts/messenger.context';
+import useInputState from '../../../hooks/useInputState';
 import SidebarPanel from './SidebarPanel';
 import FriendsListItem from './FriendsListItem';
 import '../../../styles/Sidebar.css';
 import findFriends from '../../../assets/images/friends-chat2.png';
+import search from '../../../assets/svg/search.svg';
 
 function Sidebar({userId}) {
     const {token} = useContext(AuthenticationContext);
     const {socket} = useContext(SocketContext);
     const {chatboxUser, conversations, setConversations, friends} = useContext(MessengerContext);
+    const [searchQuery, setSearchQuery, resetSearchQuery] = useInputState('', false);
+    const [searchResult, setSearchResult] = useState([]);
+    const [searchBarIsOpen, setSearchBarIsOpen] = useState(false);
 
     // Load user's conversations/direct messages
     useEffect(() => {
@@ -55,28 +61,85 @@ function Sidebar({userId}) {
         }, 1000);
     }, [socket, chatboxUser, friends]);
 
+    // Search Conversations
+    const toggleSearchBar = e => {
+        setSearchBarIsOpen(currState => !currState);
+        resetSearchQuery();
+    };
+    Sidebar.handleClickOutside = () => {
+        setSearchBarIsOpen(false);
+        resetSearchQuery();
+    };
+
+    useEffect(() => {
+        if(conversations && conversations.length > 0 && searchQuery !== '') {
+            const searchResults = conversations.filter(c => (
+                c.username.toUpperCase().search(searchQuery.toUpperCase()) !== -1
+            ));
+            console.log(searchResults);
+            setSearchResult(searchResults);
+        }
+    }, [searchQuery]);
+
+    let conversationsList;
+    if(searchQuery !== '' && searchResult.length > 0) {
+        conversationsList = (
+            searchResult.map(c => (
+                <React.Fragment key={c._id}>
+                    <FriendsListItem 
+                        userId={userId}
+                        userData={c}
+                        selected={(chatboxUser && chatboxUser._id === c._id) ? true : false}
+                    />
+                </React.Fragment>
+            ))
+        );
+    } else if(searchQuery !== '' && searchResult.length === 0) {
+        conversationsList = (
+            <div className="find-friends">
+                <img className="Sidebar-illustration" src={search} alt="No Results Found"/>
+                <p style={{textAlign: 'center'}}>
+                    No conversations found!
+                </p>
+            </div>
+        )
+    } else if(searchQuery === '' && conversations && conversations.length > 0) {
+        conversationsList = (
+            conversations.map(c => (
+                <React.Fragment key={c._id}>
+                    <FriendsListItem 
+                        userId={userId}
+                        userData={c}
+                        selected={(chatboxUser && chatboxUser._id === c._id) ? true : false}
+                    />
+                </React.Fragment>
+            ))
+        );
+    } 
+
     return (
         <nav className="Sidebar">
             <SidebarPanel />
             <div className="Sidebar-friends">
                 <span>
                     <h1 style={{display: 'inline-block', marginRight: '1rem'}}>Conversations</h1>
-                    <i className="fas fa-search sidebar-icon-1"></i>
+                    <span className="search-bar">
+                        <i className="fas fa-search sidebar-icon-1" onClick={toggleSearchBar}></i>
+                        <input 
+                            type="text" 
+                            placeholder="Search conversations..." 
+                            value={searchQuery}
+                            onChange={setSearchQuery}
+                            className={searchBarIsOpen ? 'search-bar-open' : ''}
+                        />
+                    </span>
                 </span>
                 <div className="friends-list">
                     {
                         (conversations && conversations.length > 0) 
                         ?
                         <ul style={{marginTop: '1rem'}}>
-                            {conversations.map(c => (
-                                <React.Fragment key={c._id}>
-                                    <FriendsListItem 
-                                        userId={userId}
-                                        userData={c}
-                                        selected={(chatboxUser && chatboxUser._id === c._id) ? true : false}
-                                    />
-                                </React.Fragment>
-                            ))}
+                            {conversationsList}
                         </ul>
                         :
                         (
@@ -94,4 +157,8 @@ function Sidebar({userId}) {
     )
 }
 
-export default Sidebar;
+const clickedOutsideConfig = {
+    handleClickOutside: () => Sidebar.handleClickOutside
+};
+
+export default onClickOutside(Sidebar, clickedOutsideConfig);
