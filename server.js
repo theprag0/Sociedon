aa28ebfc55fef00c3d62f3a968de496b7b0b1a4b
Praghadiesh <praghadieshnikhil234@gmail.com>
2021-08-17 19:54:28 +0530

    add content security policy directives

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ const socketMain = require('./sockets/socketMain');
 let app = express();
 app.use(express.json({limit: '50mb'}));
 app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        "default-src": ["'self'"],
+        "connect-src": ["'self'", "'unsafe-inline'"],
+        "img-src": ["'self'", "data:"],
+    }
+}));
 app.use(hpp());
 
 // Mongo Connection
